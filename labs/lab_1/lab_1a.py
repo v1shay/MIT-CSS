4ae15522172e1ece30bcb2765c2501138a117d3d
Chris Lai <chrisclai02@gmail.com>
2026-02-02 23:29:07 -0500

    Update lab_1a.py

---
+++ labs/lab_1/lab_1a.py
@@ -0,0 +1,16 @@
+"""
+lab_1a.py
+
+The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
+with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+"""
+
+def main():
+    print("Hello World!")
+
+    name = "" # TODO: Insert your name between the double quotes
+
+    print(f"{name}, Welcome to the CSS course!")
+
+if __name__ == "__main__":
+    main()
